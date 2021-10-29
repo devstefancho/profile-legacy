dacbcdd39a3eff333807d8aab3125c5f860900ee
devstefancho <skku7714@gmail.com>
2021-10-29 22:18:52 +0900

    Updates

--- static/js/main.ea8788d4.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgithub-pages"]=this["webpackJsonpgithub-pages"]||[]).push([[0],{9:function(e,t,n){"use strict";n.r(t);var s=n(3),c=n.n(s),r=n(0);var i=function(){return Object(r.jsx)("div",{children:Object(r.jsx)("h1",{children:"TEST PAGES"})})};c.a.render(Object(r.jsx)(i,{}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.ea8788d4.chunk.js.map
