cc6f52ec2eb2e6d013f1c607e6580851da2749b4
devstefancho <skku7714@gmail.com>
2021-10-29 22:20:40 +0900

    add test router pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import Details from './components/Details';
 function App() {
   return (
     <div>
-      <h1>TEST PAGES</h1>
+      <h1>StefanCho Profile</h1>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/details'>Details</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path='/details'>
+              <Details />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Details.tsx
@@ -0,0 +1,9 @@
+const Details: React.FC = () => {
+  return (
+    <div>
+      <h2>Detail....</h2>
+    </div>
+  );
+};
+
+export default Details;
