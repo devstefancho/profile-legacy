115276cacc5f7f4132bc55e14078190c7907c127
devstefancho <skku7714@gmail.com>
2021-11-07 23:59:40 +0900

    add mailto, refactoring Component
    - add mailto to gmail img src
    - refactoring to new component(renderIcon)

--- src/components/profile/Icons.tsx
+++ src/components/profile/Icons.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { ReactElement, useState } from 'react';
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { OverlayChildren } from 'react-bootstrap/esm/Overlay';
 import { Github, Blog, Gmail, Stackoverflow } from '../../svg';
@@ -6,6 +6,22 @@ import url from '../../json/url.json';
 
 const initialMessage = 'Copy to Clipboard';
 const copied = 'Copied!';
+
+interface RenderIcon {
+  href: string;
+  src: string;
+  alt?: string;
+}
+
+const RenderIcon: React.FC<RenderIcon> = ({ href, src, alt }) => {
+  return (
+    <div className='icon'>
+      <a href={href} target='_blank'>
+        <img src={src} alt={alt} />
+      </a>
+    </div>
+  );
+};
 
 export const Icons: React.FC = () => {
   const [message, setMessage] = useState<string>(initialMessage);
@@ -15,7 +31,7 @@ export const Icons: React.FC = () => {
     setMessage(copied);
   };
 
-  const onMouseLeaveIcon = () => {
+  const onMouseLeaveIcon: React.MouseEventHandler<HTMLDivElement> = () => {
     setTimeout(() => setMessage(initialMessage), 500);
   };
 
@@ -29,30 +45,18 @@ export const Icons: React.FC = () => {
 
   return (
     <div className='icon-container d-flex justify-content-between'>
-      <div className='icon'>
-        <a href={url.github} target='_blank'>
-          <img src={Github} alt='github' />
-        </a>
-      </div>
-      <div className='icon'>
-        <a href={url.blog} target='_blank'>
-          <img src={Blog} alt='blog' />
-        </a>
-      </div>
+      <RenderIcon href={url.github} src={Github} alt='github' />
+      <RenderIcon href={url.blog} src={Blog} alt='blog' />
       <OverlayTrigger placement='top-start' overlay={renderToolTip}>
-        <div
-          className='icon'
-          onClick={onClickCopyGmail}
-          onMouseLeave={onMouseLeaveIcon}
-        >
-          <img src={Gmail} alt='gmail' />
+        <div onClick={onClickCopyGmail} onMouseLeave={onMouseLeaveIcon}>
+          <RenderIcon href={`mailto:${url.gmail}`} src={Gmail} alt='gmail' />
         </div>
       </OverlayTrigger>
-      <div className='icon'>
-        <a href={url.stackoverflow} target='_blank'>
-          <img src={Stackoverflow} alt='stackoverflow' />
-        </a>
-      </div>
+      <RenderIcon
+        href={url.stackoverflow}
+        src={Stackoverflow}
+        alt='stackoverflow'
+      />
     </div>
   );
 };
