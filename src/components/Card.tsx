d9fbb7ab2af70672aa97b0aef6c30cf54b56b75a
devstefancho <skku7714@gmail.com>
2021-11-10 00:27:46 +0900

    add new data type to desc (period, url)

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,36 @@
+interface DescItem {
+  content: string;
+  period?: string;
+  url?: string;
+}
+
 interface CardProps {
   id: string;
   title: string;
-  desc: Array<string>;
+  desc: DescItem[];
 }
 
 export const Card: React.FC<CardProps> = ({ id, title, desc }) => {
-  const renderDesc = desc.map((item: string) => <div key={item}>{item}</div>);
+  const renderDesc = desc.map((item) => {
+    if (item.url) {
+      return (
+        <a key={title} href={item.url} target='_blank'>
+          <div>{item.content}</div>
+        </a>
+      );
+    }
+
+    if (item.period) {
+      return (
+        <div>
+          <div className=''>{item.period}</div>
+          <div className='fw-bold'>{item.content}</div>
+        </div>
+      );
+    }
+    return <div key={title}>{item.content}</div>;
+  });
+
   return (
     <div className='card-box'>
       <section className='card-title'>{title}</section>
